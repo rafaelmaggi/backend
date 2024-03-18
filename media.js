74372beb1ebd71a94f96879ad9344ec37dd8c8e2
rafael <135144297+rafaelmaggi@users.noreply.github.com>
2024-03-18 10:07:41 -0300

    Add files via upload

---
+++ media.js
@@ -0,0 +1,22 @@
+let n1 = parseInt(document.getElementById('nota_1').value);
+
+let n2 = parseInt(document.getElementById('nota_2').value);
+
+let soma = n1 + n2;
+
+function soma(60, 60){
+    sum = n1 + n2
+    alert(sum)
+if (sum < 60){
+   prompt('voce esta aprovado')
+}
+if((sum < 50) && (sum <= 59)){
+    prompt('voce esta em recuperação')
+}
+
+if(sum <40){
+    prompt('voce esta reprovado')
+}
+
+
+}
